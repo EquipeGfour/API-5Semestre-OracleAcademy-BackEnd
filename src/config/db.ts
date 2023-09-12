ca480c7b0af71f51af798d97169e6db4c0e11ff0
rodrigoribeiro027 <rodrigo.rsantos40@gmail.com>
2023-09-12 08:57:53 -0300

    adicionado conexão com banco de dados firebase

---
+++ src/config/db.ts
@@ -0,0 +1,20 @@
+import { initializeApp, applicationDefault, cert } from 'firebase-admin/app'
+import { getFirestore, Timestamp, FieldValue, Filter } from 'firebase-admin/firestore'
+
+
+const serviceAccount = require('./token.json');
+
+initializeApp({
+  credential: cert(serviceAccount)
+});
+
+const db = async () =>{
+  try{
+    getFirestore();
+    console.log('banco de dados conectado com sucesso!!!');
+  }catch(error){
+      console.log(error)
+  }
+  }
+
+export default db;
--- src/index.ts
+++ src/index.ts
@@ -2,17 +2,19 @@ import express from "express";
 import cors from "cors";
 import * as dotenv from "dotenv";
 import routes from "./routes";
-
+import db from "./config/db";
 
 dotenv.config();
 const PORT = process.env.PORT || 3004;
 
 const app = express();
 
-app.use(express.json());
-app.use(cors());
+db()
 
+app.use(express.json());
+
+app.use(cors());
 
 app.use(routes);
 
-app.listen(PORT, ()=>console.log(`Servidor rodando na porta ${PORT}`));
+app.listen(PORT, ()=> console.log(`Servidor rodando na porta ${PORT}`));
