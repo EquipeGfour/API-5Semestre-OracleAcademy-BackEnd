723d17e6d06765215796e745521b1de55dca528f
Nicolas <nichollaslimma734@gmail.com>
2023-09-16 12:52:35 -0300

    criado rota, controller e service para realizar o cadastro de novas tarefas em um objetivo

--- src/controllers/TarefaController.ts
+++ src/controllers/TarefaController.ts
@@ -1,5 +1,30 @@
-class TarefaController{
+import { Request, Response } from "express";
+import { TarefaService } from "../services";
+import { Tarefas } from "../models";
+import { v4 as uuidv4 } from "uuid";
+import { STATUS } from "../utils/enum";
 
+
+class TarefaController{
+    public async criarTarefa(req: Request, res: Response){
+        try{
+            const {id} = req.params;
+            const tarefa: Tarefas = req.body;
+            let idTarefa: number = uuidv4();
+            tarefa.id = idTarefa.toString();
+            tarefa.data_criacao = Date.now().toString();
+            tarefa.data_estimada = "";
+            tarefa.data_inicio = "";
+            tarefa.status = STATUS.NAO_INICIADO;
+            tarefa.anexo = false;
+            tarefa.arquivos = null;
+            const response: Tarefas[] = await TarefaService.createTarefa(id, tarefa);
+            res.json(response);
+        }catch(error){
+            console.log(error)
+            res.status(500).json(error);
+        }
+    }
 }
 
 export default new TarefaController();
--- src/services/TarefaService.ts
+++ src/services/TarefaService.ts
@@ -1,5 +1,25 @@
-class TarefaService{
+import { connection } from "../config/db";
+import { Objetivos, Tarefas } from "../models";
 
+
+class TarefaService {
+    public async createTarefa(id: string, tarefa: Tarefas): Promise<Tarefas[]> {
+        try {
+            let response = await connection.collection("objetivos").doc(id);
+            let values = await response.get();
+            if (values.exists) {
+                const listaObjetivos: Tarefas[] = (values.data() as Objetivos).tarefas;
+                listaObjetivos.push(tarefa);
+                console.log(listaObjetivos)
+                response.update({ tarefas: listaObjetivos })
+                return listaObjetivos;
+            } else {
+                throw `Objetivo ${id} não encontrado...`
+            }
+        } catch (error) {
+            throw error
+        }
+    }
 }
 
 
