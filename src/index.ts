7e0ad4866ae9f0dde2824f59c2254e585054cc7d
Nicolas <nichollaslimma734@gmail.com>
2023-09-25 10:36:54 -0300

    criado conexão com o banco de dados mongodb

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,21 @@
-import express from "express";
 import cors from "cors";
+import express from "express";
 import * as dotenv from "dotenv";
-import routes from "./routes";
-import db from "./config/db";
+// import routes from "./routes";
+import startDb from "./config/db";
 import swaggerUi from 'swagger-ui-express';
 import swaggerDocument from './swagger/swagger_output.json';
 
+
 dotenv.config();
+const app = express();
 const PORT = process.env.PORT || 3004;
 
-const app = express();
-
-db()
+startDb();
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-
 app.use(express.json());
-
 app.use(cors());
-
-app.use(routes);
+// app.use(routes);
 
 app.listen(PORT, ()=> console.log(`Servidor rodando na porta ${PORT}`));
