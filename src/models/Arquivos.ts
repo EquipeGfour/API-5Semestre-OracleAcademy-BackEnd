b91eb6336791b227bcffcb7484139dca7d1c921e
Nicolas <nichollaslimma734@gmail.com>
2023-10-03 20:37:42 -0300

    criado modelo do arquivo

--- src/models/Arquivos.ts
+++ src/models/Arquivos.ts
@@ -1,6 +1,18 @@
-export default interface Arquivos {
-    id?: String | null,
-    nome: String,
-    url: String,
-    tipo: String
+import mongoose from "mongoose";
+
+
+const { Schema } = mongoose;
+
+interface IArquivos {
+    nome: string,
+    ext: string,
+    url: string
 }
+
+const Arquivos = new Schema({
+    nome: String,
+    ext: String,
+    url: String
+})
+
+export { IArquivos, Arquivos };
--- src/models/index.ts
+++ src/models/index.ts
@@ -1,6 +1,7 @@
-import Arquivos from "./Arquivos";
+import { Arquivos, IArquivos } from "./Arquivos";
 import { Objetivo, IObjetivo } from "./Objetivos";
 import { Tarefa, ITarefa } from "./Tarefas";
 import Usuario from "./Usuario";
 
-export { Objetivo, IObjetivo, Tarefa, ITarefa, Usuario, Arquivos}
+
+export { Objetivo, IObjetivo, Tarefa, ITarefa, Usuario, Arquivos, IArquivos};
