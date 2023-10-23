5e9c828d3f140bf14a83984a37df6269c30b7e95
Nicolas <nichollaslimma734@gmail.com>
2023-10-23 20:44:43 -0300

    adicionado autenticate a rota de upload

--- src/routes/uploadRouter.ts
+++ src/routes/uploadRouter.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { UploadController } from "../controllers";
+import { authenticate } from "../middlewares";
 
 
 const routes = Router();
 
 routes.post("/usuario", UploadController.uploadUserFile);
-routes.post("/tarefa/:id", UploadController.uploadFileFromTask);
+routes.post("/tarefa/:id", authenticate, UploadController.uploadFileFromTask);
 
 
 export default routes;
