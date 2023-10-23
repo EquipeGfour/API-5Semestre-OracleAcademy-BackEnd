1a4103a46800d97ec245c85a9c90886e94a36cce
Nicolas <nichollaslimma734@gmail.com>
2023-10-23 20:38:58 -0300

    adicionado validação para o upload de arquivo da tarefa

--- src/controllers/UploadController.ts
+++ src/controllers/UploadController.ts
@@ -27,29 +27,45 @@ class UploadController{
         }
     }
 
-    public async uploadFileFromTask(req: Request, res: Response){
-        try{
+    
+    public async uploadFileFromTask(req: Request, res: Response) {
+        try {
             const { id } = req.params;
             const task = await TarefaService.findTaskByID(id);
-            let body = req.files.file;
-            if(Array.isArray(body)){
-                body = body[0]
+
+            //verifica se o campo "file" esta sendo enviado
+            if (!req.files) {
+                return res.status(400).json("Campo file não recebido.");
             }
-            const file = extractDataFromFile(body);
+
+            //verifica se esta sendo enviado algum arquivo
+            const [file] = Array.isArray(req.files.file) ? req.files.file : [req.files.file];
+            if (!file) {
+                return res.status(400).json("Nenhum arquivo recebido.");
+            }
+
+            //extrai as informações do arquivo
+            const fileData = extractDataFromFile(file);
             const drive = DriveController.DriveInstance;
+
+            //busca pasta "task" no drive e verifica se ela foi encontrada
             const folders = await drive.searchFolder("tasks");
-            if(folders.length < 1){
-                return res.status(404).json(`Pasta users não encontrada.`);
+            if (folders.length < 1) {
+                return res.status(404).json("Pasta tasks não encontrada.");
             }
-            const response = await drive.sendFile(file.fileName, file.mimeType, file.fileContent, folders[0].id);
-            const arquivo: IArquivos = buildUploadObject(file.fileName, file.ext, response.data.id);
+
+            //envia arquivo para o drive
+            const response = await drive.sendFile(fileData.fileName, fileData.mimeType, fileData.fileContent, folders[0].id);
+
+            //monta o objeto do arquivo e adiciona na listad e arquivos para atualizar a tarefa
+            const arquivo: IArquivos = buildUploadObject(fileData.fileName, fileData.ext, response.data.id);
             task.anexo = true;
             task.arquivos.push(arquivo);
-            await Tarefa.updateOne({_id:task._id}, {$set:{arquivos: task.arquivos}});
-            return res.json(`arquivo enviado com sucesso!`);
-        }catch(error){
+            await Tarefa.updateOne({ _id: task._id }, { $set: { arquivos: task.arquivos } });
+            return res.json("Arquivo enviado com sucesso!");
+        } catch (error) {
             console.error(error);
-            res.status(500).json(error);
+            res.status(500).json(error.message);
         }
     }
 
