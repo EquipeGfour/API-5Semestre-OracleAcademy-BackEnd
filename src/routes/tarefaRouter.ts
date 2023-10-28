2f5221c84fbd477d22465209935602f6e53c3864
Nicolas Lima <88324308+Nicolas734@users.noreply.github.com>
2023-10-28 14:50:12 -0300

    atualizado rota de atualizar

--- src/routes/tarefaRouter.ts
+++ src/routes/tarefaRouter.ts
@@ -15,7 +15,7 @@ routes.put("/adicionarUser/:id", TarefaController.adicionarUsuariosTarefa);
 routes.put("/mudarStatus/:id", TarefaController.mudarStatusDaTarefa);
 routes.get("/buscarTarefasStatus", authenticate, TarefaController.buscarTarefaStatus);
 routes.get("/buscarTarefasExpiradas", authenticate, TarefaController.buscarTarefasExpiradas);
-routes.put("/cronometro/atualizar/:id", TarefaController.atualizarCronometro);
+routes.put("/atualizarCronometro/:id", TarefaController.atualizarCronometro);
 routes.get('/cronometro/buscar/:id', TarefaController.buscarCronometro);
 
 export default routes;
